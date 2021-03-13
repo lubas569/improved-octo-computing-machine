f5e85c3ffda3bdfa734818507fa1e6369d44a3c2
Lubas569 <52548763+lubas569@users.noreply.github.com>
2021-03-13 17:10:26 +0000

    Initialize standalone_autoscheduler_hexagon

---
+++ apps/stencil_chain/process.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <chrono>
+
+#include "stencil_chain.h"
+#include "stencil_chain_classic_auto_schedule.h"
+#include "stencil_chain_auto_schedule.h"
+
+#include "benchmark_util.h"
+#include "HalideBuffer.h"
+#include "halide_image_io.h"
+
+using namespace Halide::Runtime;
+using namespace Halide::Tools;
+
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        printf("Usage: ./process input.png timing_iterations output.png\n"
+               "e.g.: ./process input.png 10 output.png\n");
+        return 0;
+    }
+
+    // Input may be a PNG8
+    Buffer<uint16_t> input = load_and_convert_image(argv[1]);
+    // Just take the red channel
+    input.slice(2, 0);
+
+    Buffer<uint16_t> output(input.width(), input.height());
+
+    three_way_bench(
+        [&]() { stencil_chain(input, output); },
+        [&]() { stencil_chain_classic_auto_schedule(input, output); },
+        [&]() { stencil_chain_auto_schedule(input, output); }
+    );
+
+    stencil_chain(input, output);
+    convert_and_save_image(output, argv[3]);
+
+    return 0;
+}
