1101973b3c171cc4b7c280cc978f43372c1c5e72
Lubas569 <52548763+lubas569@users.noreply.github.com>
2021-03-13 17:07:40 +0000

    Initialize standalone_autoscheduler

---
+++ apps/lens_blur/process.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <chrono>
+
+#include "lens_blur.h"
+#include "lens_blur_classic_auto_schedule.h"
+#include "lens_blur_auto_schedule.h"
+
+#include "benchmark_util.h"
+#include "HalideBuffer.h"
+#include "halide_image_io.h"
+
+using namespace Halide::Runtime;
+using namespace Halide::Tools;
+
+int main(int argc, char **argv) {
+    if (argc < 7) {
+        printf("Usage: ./process input.png slices focus_depth blur_radius_scale aperture_samples timing_iterations output.png\n"
+               "e.g.: ./process input.png 32 13 0.5 32 3 output.png\n");
+        return 0;
+    }
+
+    Buffer<uint8_t> left_im = load_image(argv[1]);
+    Buffer<uint8_t> right_im = load_image(argv[1]);
+    uint32_t slices = atoi(argv[2]);
+    uint32_t focus_depth = atoi(argv[3]);
+    float blur_radius_scale = atof(argv[4]);
+    uint32_t aperture_samples = atoi(argv[5]);
+    Buffer<float> output(left_im.width(), left_im.height(), 3);
+
+    // Timing code
+    three_way_bench(
+        [&]() { lens_blur(left_im, right_im, slices, focus_depth, blur_radius_scale, aperture_samples, output); },
+        [&]() { lens_blur_classic_auto_schedule(left_im, right_im, slices, focus_depth, blur_radius_scale, aperture_samples, output); },
+        [&]() { lens_blur_auto_schedule(left_im, right_im, slices, focus_depth, blur_radius_scale, aperture_samples, output); }
+    );
+
+    convert_and_save_image(output, argv[7]);
+
+    return 0;
+}
---
+++ apps/local_laplacian/process.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <chrono>
+
+#include "local_laplacian.h"
+#ifndef NO_AUTO_SCHEDULE
+#include "local_laplacian_classic_auto_schedule.h"
+#include "local_laplacian_auto_schedule.h"
+#endif
+
+#include "benchmark_util.h"
+#include "HalideBuffer.h"
+#include "halide_image_io.h"
+
+using namespace Halide::Runtime;
+using namespace Halide::Tools;
+
+int main(int argc, char **argv) {
+    if (argc < 7) {
+        printf("Usage: ./process input.png levels alpha beta timing_iterations output.png\n"
+               "e.g.: ./process input.png 8 1 1 10 output.png\n");
+        return 0;
+    }
+
+    // Input may be a PNG8
+    Buffer<uint16_t> input = load_and_convert_image(argv[1]);
+
+    int levels = atoi(argv[2]);
+    float alpha = atof(argv[3]), beta = atof(argv[4]);
+    Buffer<uint16_t> output(input.width(), input.height(), 3);
+
+    three_way_bench(
+        [&]() { local_laplacian(input, levels, alpha/(levels-1), beta, output); },
+    #ifdef NO_AUTO_SCHEDULE
+        nullptr,
+        nullptr
+    #else
+        [&]() { local_laplacian_classic_auto_schedule(input, levels, alpha/(levels-1), beta, output); },
+        [&]() { local_laplacian_auto_schedule(input, levels, alpha/(levels-1), beta, output); }
+    #endif
+    );
+
+    convert_and_save_image(output, argv[6]);
+
+    return 0;
+}
